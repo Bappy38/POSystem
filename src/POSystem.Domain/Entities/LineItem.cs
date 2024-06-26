10b7a09afd6149237eafd544e680982422a6f772
Bappy38 <iqbal.bappy.13@gmail.com>
2024-06-26 11:44:50 +0600

    Models created

---
+++ src/POSystem.Domain/Entities/LineItem.cs
@@ -0,0 +1,11 @@
+﻿namespace POSystem.Domain.Entities;
+
+public class LineItem
+{
+    public int Id { get; set; }
+    public int OrderId { get; set; }
+    public int Name { get; set; }
+    public int Quantity { get; set; }
+    public decimal Rate { get; set; }
+    public Order Order { get; set; }
+}
---
+++ src/POSystem.Domain/Entities/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace POSystem.Domain.Entities;
+
+public class Order
+{
+    public int Id { get; set; }
+    public string ReferenceId { get; set; }
+    public DateTime Date { get; set; }
+    public Supplier Supplier { get; set; }
+    public DateTime? ExpectedDate { get; set; }
+    public string Remark { get; set; }
+    public ICollection<LineItem> Items { get; set; }
+}
