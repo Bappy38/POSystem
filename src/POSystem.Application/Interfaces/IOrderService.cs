42c1a7c9bb86a9aa566a6b3398b8193624892f3b
Bappy38 <iqbal.bappy.13@gmail.com>
2024-06-26 21:05:25 +0600

    Order Service Implemented

---
+++ src/POSystem.Application/Interfaces/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using POSystem.Domain.DTOs;
+using POSystem.Domain.Entities;
+
+namespace POSystem.Application.Interfaces;
+
+public interface IOrderService
+{
+    Task<int> CreateAsync(OrderDto order);
+    Task<int> UpdateAsync(OrderDto order);
+    Task<int> DeleteAsync(int id);
+    Task<OrderDto> GetByIdAsync(int id);
+    Task<List<Order>> GetPagedAsync(int cursor, int pageSize);
+}
---
+++ src/POSystem.Application/Services/OrderService.cs
@@ -0,0 +1,41 @@
+﻿using POSystem.Application.Interfaces;
+using POSystem.Domain.DTOs;
+using POSystem.Domain.Entities;
+using POSystem.Domain.Repositories;
+
+namespace POSystem.Application.Services;
+
+public class OrderService : IOrderService
+{
+    private readonly IOrderRepository orderRepository;
+
+    public OrderService(IOrderRepository orderRepository)
+    {
+        this.orderRepository = orderRepository;
+    }
+
+    public async Task<int> CreateAsync(OrderDto order)
+    {
+        return await orderRepository.CreateAsync(order);
+    }
+
+    public async Task<int> DeleteAsync(int id)
+    {
+        return await orderRepository.DeleteAsync(id);
+    }
+
+    public async Task<OrderDto> GetByIdAsync(int id)
+    {
+        return await orderRepository.GetByIdAsync(id);
+    }
+
+    public async Task<List<Order>> GetPagedAsync(int cursor, int pageSize)
+    {
+        return await orderRepository.GetPagedAsync(cursor, pageSize);
+    }
+
+    public async Task<int> UpdateAsync(OrderDto order)
+    {
+        return await orderRepository.UpdateAsync(order);
+    }
+}
