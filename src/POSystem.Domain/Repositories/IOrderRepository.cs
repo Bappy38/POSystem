8bf06009e44c22466737fd09b3494f4fef026863
Bappy38 <iqbal.bappy.13@gmail.com>
2024-06-26 11:46:02 +0600

    Interface created for OrderRepository

---
+++ src/POSystem.Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using POSystem.Domain.Entities;
+
+namespace POSystem.Domain.Repositories;
+
+public interface IOrderRepository
+{
+    Task<IEnumerable<Order>> GetAllAsync();
+    Task<Order> GetByIdAsync(int id);
+    Task AddAsync(Order order);
+    Task UpdateAsync(Order order);
+    Task DeleteAsync(int id);
+}
