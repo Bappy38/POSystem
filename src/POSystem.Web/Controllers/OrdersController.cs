c833bffdb9e52ea89a771d9b82efdce1704cca1d
Bappy38 <iqbal.bappy.13@gmail.com>
2024-06-29 21:02:29 +0600

    Suppliers dropdown added in create action

--- src/POSystem.Web/Controllers/OrdersController.cs
+++ src/POSystem.Web/Controllers/OrdersController.cs
@@ -1,27 +1,32 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using POSystem.Application.Constants;
 using POSystem.Application.Interfaces;
 using POSystem.Domain.DTOs;
 using POSystem.Domain.Entities;
 using POSystem.Web.Constants;
+using POSystem.Web.Models;
 
 namespace POSystem.Web.Controllers;
 
 public class OrdersController : Controller
 {
     private readonly IOrderService _orderService;
-    private readonly IMapper _mapper;
     private readonly IOrderExporter _orderExporter;
+    private readonly ISupplierService _supplierService;
+    private readonly IMapper _mapper;
 
     public OrdersController(
-        IOrderService orderService, 
-        IMapper mapper, 
-        IOrderExporter orderExporter)
+        IOrderService orderService,  
+        IOrderExporter orderExporter,
+        ISupplierService supplierService,
+        IMapper mapper)
     {
         _orderService = orderService;
-        _mapper = mapper;
         _orderExporter = orderExporter;
+        _supplierService = supplierService;
+        _mapper = mapper;
     }
 
     public async Task<IActionResult> Index(int pageNo = 1, int pageSize = 10, string searchQuery = "")
@@ -32,21 +37,39 @@ public class OrdersController : Controller
         return View(orders);
     }
 
-    public IActionResult Create()
+    public async Task<IActionResult> Create()
     {
-        return View();
+        var suppliers = await _supplierService.GetSuppliersAsync();
+        var orderViewModel = new CreateOrderViewModel
+        {
+            OrderDto = new CreateOrderDto(),
+            Suppliers = suppliers.Select(s => new SelectListItem
+            {
+                Value = s.Id.ToString(),
+                Text = s.Name
+            }).ToList()
+        };
+
+        return View(orderViewModel);
     }
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create(CreateOrderDto order)
+    public async Task<IActionResult> Create(CreateOrderViewModel createOrderViewModel)
     {
         if (!ModelState.IsValid)
         {
-            return View(order);
+            var suppliers = await _supplierService.GetSuppliersAsync();
+            createOrderViewModel.Suppliers = suppliers.Select(s => new SelectListItem
+            {
+                Value = s.Id.ToString(),
+                Text = s.Name
+            }).ToList();
+
+            return View(createOrderViewModel);
         }
 
-        var result = await _orderService.CreateAsync(order);
+        var result = await _orderService.CreateAsync(createOrderViewModel.OrderDto);
 
         var notificationType = result ? Notification.Success : Notification.Error;
         var notificationMessage = result
---
+++ src/POSystem.Web/Models/CreateOrderViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using POSystem.Domain.DTOs;
+using POSystem.Domain.Entities;
+
+namespace POSystem.Web.Models;
+
+public class CreateOrderViewModel
+{
+    public CreateOrderDto OrderDto { get; set; }
+    public List<SelectListItem> Suppliers { get; set; } = new List<SelectListItem>();
+}
